e8ab41e879393b1d3a286f81c78d9c39bccccf78
zjj2442542687 <2442542687@qq.com>
2020-07-16 14:22:05 +0800

    20200716_zjj_200改回自定义

--- user/views/user_insert.py
+++ user/views/user_insert.py
@@ -34,6 +34,13 @@ class UserInsertView(mixins.CreateModelMixin,
     serializer_class = UserInfoSerializers
 
     @swagger_auto_schema(
+        # request_body=openapi.Schema(
+        #     type=openapi.TYPE_OBJECT,
+        #     properties={
+        #         'code': openapi.Schema(type=openapi.TYPE_STRING, description="这是date"),
+        #         'username': openapi.Schema(type=openapi.TYPE_INTEGER),
+        #     }
+        # ),
         request_body=request_body(properties={
             'user_name': string_schema('用户名'),
             'password': string_schema('密码'),
@@ -41,6 +48,9 @@ class UserInsertView(mixins.CreateModelMixin,
             'code': string_schema('验证码')
         })
     )
+    # request_body=request_body(property={
+    #             "code": string_schema("123")
+    #         }),
     def create(self, request, *args, **kwargs):
         message = 'SUCCESS'
         user_name = request.data.get('user_name')
@@ -54,16 +64,19 @@ class UserInsertView(mixins.CreateModelMixin,
 
             if check_user_name(user_name):
                 message = "用户名已存在"
-                return response(code=STATUS_USER_NAME_DUPLICATE, message=message)
+                return Response({"status": STATUS_USER_NAME_DUPLICATE, "message": message})
+                return response_error_400(status=STATUS_USER_NAME_DUPLICATE, message=message)
             if check_phone_number(phone_number):
                 message = "该手机号已被注册"
-                return response(code=STATUS_PHONE_NUMBER_DUPLICATE, message=message)
+                return Response({"status": STATUS_PHONE_NUMBER_DUPLICATE, "message": message})
+                return response_error_400(status=STATUS_PHONE_NUMBER_DUPLICATE, message=message)
             if not judge_code(phone_number, request.data.get('code')):
                 message = "验证码不正确"
-                return response(code=STATUS_CODE_ERROR, message=message)
+                return Response({"status": STATUS_CODE_ERROR, "message": message})
+                return response_error_400(status=STATUS_CODE_ERROR, message=message)
 
         except UserWarning:
-            return response(code=STATUS_PHONE_NUMBER_ERROR, message=message)
+            return response_success_200(status=STATUS_PHONE_NUMBER_ERROR, message=message)
 
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
