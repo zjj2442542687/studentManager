b264bb25f2b4fbd6eedde338ef8ee18ff5fd21b1
zjj2442542687 <2442542687@qq.com>
2020-07-16 15:52:55 +0800

    20200716_zjj_student查询

--- student/urls.py
+++ student/urls.py
@@ -4,17 +4,14 @@ from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from django.contrib import admin
 
-from student.views import StudentInsertView
-from student.views import StudentOtherView
-from student.views import StudentSelectView
-from user.views.user_insert import UserInsertView
-from user.views.user_other import UserOtherView
-from user.views.user_select import UserSelectView
+from student.views import StudentInsertView, StudentOtherView, StudentSelectView
+
 
 urlpatterns = [
     path("insert", StudentInsertView.as_view({'post': 'create'})),
     path("getAll", StudentSelectView.as_view({'get': 'list'})),
     path("getStudentById/<int:pk>", StudentSelectView.as_view({'get': 'retrieve'})),
+    path("getStudentByName/<str:student_name>", StudentInsertView.as_view({'get': 'retrieve_by_studentname'})),
     path("update/<int:pk>", StudentOtherView.as_view({'patch': 'update'})),
-    # path("delete/<int:pk>", UserOtherView.as_view({'delete': 'destroy'})),
+    path("delete/<int:pk>", StudentOtherView.as_view({'delete': 'destroy'})),
 ]
--- student/views.py
+++ student/views.py
@@ -9,6 +9,8 @@ from rest_framework import serializers, mixins, status
 from rest_framework.serializers import ModelSerializer
 
 from student.models import Student
+from utils.my_response import *
+from utils.my_swagger_auto_schema import *
 
 
 class StudentInfoSerializers(ModelSerializer):
@@ -34,10 +36,20 @@ class StudentOtherView(ModelViewSet):
     """
     update:修改学生表信息
 
-    无描述
+    根据id修改用户信息
+
+    输入id修改
+
+    destroy:删除学生信息
+
+    根据id删除用户信息
+
+    输入id删除
+
     """
     queryset = Student.objects.all()
     serializer_class = StudentInfoSerializers
+
 
 
 class StudentInfoSerializers2(ModelSerializer):
@@ -61,6 +73,27 @@ class StudentSelectView(mixins.ListModelMixin,
 
     输入id
 
+    retrieve_by_studentname:
+    根据名字查询学生信息
+
+    输入姓名
+
     """
     queryset = Student.objects.all()
     serializer_class = StudentInfoSerializers2
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        serializer = self.get_serializer(queryset, many=True)
+        return response_success_200(data=serializer.data)
+
+    def retrieve_by_studentname(self, request, *args, **kwargs):
+        try:
+            instance = self.queryset.get(user_name=kwargs.get("student_name"))
+        except Student.DoesNotExist:
+            return response_error_500(status=STATUS_NOT_FOUND_ERROR, message="没找到")
+        except Student.MultipleObjectsReturned:
+            return response_error_500(status=STATUS_MULTIPLE_ERROR, message="找到多个姓名相同用户")
+        serializer = self.get_serializer(instance)
+        return response_success_200(data=serializer.data)
