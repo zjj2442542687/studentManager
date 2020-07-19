443c3cdf3fa3385276ff4839c456dbab8d9d0e74
dyx77421088 <2639074625@qq.com>
2020-07-19 19:40:30 +0800

    20200719_dyx_用户名唯一

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField("用户名", max_length=255)
+    user_name = models.CharField("用户名", max_length=255, unique=True)
     password = models.CharField("密码", max_length=255)
     phone_number = models.CharField("手机号", max_length=255, unique=True)
 
