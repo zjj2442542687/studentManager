f0ce8d2863abe4f128a546e6bcfb4f3665d68e4a
dyx77421088 <2639074625@qq.com>
2020-07-21 20:17:21 +0800

    20200721_dyx_用户详情

--- user_details/views/my_limit_offset_pagination.py
+++ user_details/views/my_limit_offset_pagination.py
@@ -5,6 +5,8 @@ class MyLimitOffsetPagination(LimitOffsetPagination):
     # 默认显示的个数
     default_limit = 5
     offset_query_param = "index"
+    offset_query_description = "开始的位置"
     limit_query_param = "size"
+    limit_query_description = "获得的个数，默认5个，最多10个"
     # 一页最多显示的个数
     max_limit = 10
