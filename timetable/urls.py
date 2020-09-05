c20a95298716d3ff45ba672a8e88fde55c6e20f3
zjj2442542687 <2442542687@qq.com>
2020-09-05 14:15:50 +0800

    20200905-zjj-insert

--- timetable/urls.py
+++ timetable/urls.py
@@ -3,6 +3,7 @@ from timetable.views.timetable_insert import TimetableInsertView
 from timetable.views.timetable_select import TimetableSelectView
 
 urlpatterns = [
+    path("insert", TimetableInsertView.as_view({'post': 'create'})),
     path("addCourse", TimetableInsertView.as_view({'post': 'add_course'})),
     path("selectClass", TimetableSelectView.as_view({'post': 'select_class'})),
     path("selectStudentTimetableByToken", TimetableSelectView.as_view({'post': 'select_student_timetable_by_token'})),
--- timetable/views/timetable_insert.py
+++ timetable/views/timetable_insert.py
@@ -1,3 +1,4 @@
+from classs.models import Class
 from course.models import Course
 from timetable.models import Timetable
 from rest_framework import mixins
@@ -6,8 +7,8 @@ from drf_yasg.utils import swagger_auto_schema
 
 from timetable.views import views
 from timetable.views.timetable_serializers import TimetableAllSerializersInsert
-from utils.my_response import response_success_200, response_error_400
-from utils.my_swagger_auto_schema import request_body, integer_schema
+from utils.my_response import response_success_200, response_error_400, STATUS_PARAMETER_ERROR
+from utils.my_swagger_auto_schema import request_body, integer_schema, string_schema
 
 
 class TimetableInsertView(mixins.CreateModelMixin,
@@ -30,3 +31,25 @@ class TimetableInsertView(mixins.CreateModelMixin,
         result = views.add_course(timetable_id, course_id)
         return result if result else response_success_200(message="添加成功")
 
+    # @swagger_auto_schema(
+    #     request_body=request_body(properties={
+    #         'class_info': integer_schema('班级id'),
+    #         'course_info': integer_schema('课程ID'),
+    #         'week': string_schema('星期'),
+    #         'Date': string_schema('时间'),
+    #     })
+    # )
+    # def create(self, request, *args, **kwargs):
+    #     class_info = request.data.get('class_info')
+    #     course_info = request.data.get('course_info')
+    #     if not Class.objects.filter(id=class_info):
+    #         message = "班级ID信息不存在"
+    #         return response_error_400(status=STATUS_PARAMETER_ERROR, message=message)
+    #     if not Course.objects.filter(id=course_info):
+    #         message = "课程不存在"
+    #         return response_error_400(status=STATUS_PARAMETER_ERROR, message=message)
+    #     # 把课程添加到课程表中
+    #     resp = super().create(request)
+    #     Timetable.objects.get(id=resp.data['id']).course_info.add(course_info)
+    #     print(resp.data)
+    #     return response_success_200(data=resp.data)
