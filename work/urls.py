6e1c920aa8a1fbdeb3dfaaa1b3c26f76a552b748
zjj2442542687 <2442542687@qq.com>
2020-09-11 16:46:45 +0800

    20200911-zjj-work查询

--- work/urls.py
+++ work/urls.py
@@ -10,6 +10,12 @@ urlpatterns = [
     path("update/<int:pk>", WorkOtherView.as_view({'patch': 'partial_update'})),
     # 查询所有作业
     path("getAll", WorkPaginationSelectView.as_view({'get': 'list'})),
+    # 管理员查询作业信息
+    path("getInfo", WorkPaginationSelectView.as_view({'get': 'search_adm'})),
+    # 老师查询自己发布作业信息
+    path("getTeacherInfo", WorkPaginationSelectView.as_view({'get': 'search_teacher'})),
+    # 辅导员查询自己班级作业信息
+    path("getClassInfo", WorkPaginationSelectView.as_view({'get': 'search_clazz'})),
     # 删除作业
     path("delete/<int:pk>", WorkOtherView.as_view({'delete': 'destroy'})),
 ]
--- work/views/work_search.py
+++ work/views/work_search.py
@@ -3,14 +3,20 @@ from drf_yasg.utils import swagger_auto_schema
 from requests import Response
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
+
+from classs.models import Class
+from teacher.models import Teacher
+from utils.my_info_judge import pd_adm_token, STATUS_TOKEN_NO_AUTHORITY
 from utils.my_limit_offset_pagination import MyLimitOffsetPagination
+from utils.my_response import response_error_400
+from utils.my_utils import get_class_all_id
 from work.models import Work
 from work.views.work_serializers import WorkSerializersSearch
 
 
 class WorkPaginationSelectView(mixins.ListModelMixin,
-                                  mixins.RetrieveModelMixin,
-                                  GenericViewSet):
+                               mixins.RetrieveModelMixin,
+                               GenericViewSet):
     queryset = Work.objects.all()
     serializer_class = WorkSerializersSearch
     pagination_class = MyLimitOffsetPagination
@@ -29,3 +35,77 @@ class WorkPaginationSelectView(mixins.ListModelMixin,
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+    @swagger_auto_schema(
+        operation_summary="管理员查询作业信息",
+        pagination_class=None,
+        manual_parameters=[
+            openapi.Parameter('clazz', openapi.IN_QUERY, type=openapi.TYPE_INTEGER,
+                              description='班级id', enum=get_class_all_id()),
+            openapi.Parameter('teacher', openapi.IN_QUERY, type=openapi.TYPE_INTEGER,
+                              description='老师id', enum=get_class_all_id()),
+            openapi.Parameter('TOKEN', openapi.IN_HEADER, type=openapi.TYPE_STRING, description='管理员TOKEN'),
+        ]
+    )
+    def search_adm(self, request, *args, **kwargs):
+        check_token = pd_adm_token(request)
+        if check_token:
+            return check_token
+
+        # 班级
+        clazz = request.GET.get("clazz")
+        work = Work.objects.filter(clazz=clazz)
+
+        # 老师
+        teacher = request.GET.get("teacher")
+        if work is not None:
+            work = Work.objects.filter(teacher=teacher)
+        else:
+            work = work.filter(teacher=teacher)
+
+        print(work)
+        page = self.paginate_queryset(work)
+        serializer = self.serializer_class(page, many=True, context=self.get_serializer_context())
+
+        return self.get_paginated_response(serializer.data)
+
+    @swagger_auto_schema(
+        operation_summary="老师查询自己发布作业信息",
+        pagination_class=None,
+        manual_parameters=[
+            openapi.Parameter('teacher', openapi.IN_QUERY, type=openapi.TYPE_INTEGER,
+                              description='老师id', enum=get_class_all_id()),
+        ]
+    )
+    def search_teacher(self, request, *args, **kwargs):
+        # 老师
+        teacher = request.GET.get("teacher")
+        work = Work.objects.filter(teacher=teacher)
+
+        print(work)
+        page = self.paginate_queryset(work)
+        serializer = self.serializer_class(page, many=True, context=self.get_serializer_context())
+
+        return self.get_paginated_response(serializer.data)
+
+    @swagger_auto_schema(
+        operation_summary="辅导员查询自己班级作业信息",
+        pagination_class=None,
+        manual_parameters=[
+            openapi.Parameter('clazz', openapi.IN_QUERY, type=openapi.TYPE_INTEGER,
+                              description='班级id', enum=get_class_all_id()),
+            openapi.Parameter('TOKEN', openapi.IN_HEADER, type=openapi.TYPE_STRING, description='管理员TOKEN'),
+        ]
+    )
+    def search_clazz(self, request, *args, **kwargs):
+        teacher_pk = Class.objects.get(pk=request.GET.get('clazz')).headmaster.pk
+        if request.user.pk is not teacher_pk:
+            return response_error_400(status=STATUS_TOKEN_NO_AUTHORITY, message="这不是您所带班级")
+        # 班级
+        clazz = request.GET.get("clazz")
+        work = Work.objects.filter(clazz=clazz)
+
+        print(work)
+        page = self.paginate_queryset(work)
+        serializer = self.serializer_class(page, many=True, context=self.get_serializer_context())
+        return self.get_paginated_response(serializer.data)
