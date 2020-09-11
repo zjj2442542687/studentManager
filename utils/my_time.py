b9b5b8a678eabfa2c4f3d3ccd39f69eec27b1318
dyx77421088 <2639074625@qq.com>
2020-09-11 19:06:54 +0800

    202009011_dyx_修改

--- utils/my_time.py
+++ utils/my_time.py
@@ -17,17 +17,17 @@ def time_stamp_to_str(time_stamp=time.time(), date_format="%Y-%m-%d %H:%M:%S"):
 
 # 数据库保存的时间最小的取值
 def get_min_time_stamp():
-    return date_to_time_stamp(year=2010)
+    return date_to_time_stamp(year=1910)
 
 
 # 数据库保存的时间最大的取值
 def get_max_time_stamp():
-    return date_to_time_stamp(year=2030)
+    return date_to_time_stamp(year=2050)
 
 
 # 检测时间戳是否在范围内
 def check_time_stamp(time_stamp):
     if time_stamp < get_min_time_stamp():
-        return f"输入的时间戳需要在2010年({date_to_time_stamp(year=2010)})之后"
+        return f"输入的时间戳需要在1910年({date_to_time_stamp(year=1910)})之后"
     if time_stamp > get_max_time_stamp():
-        return f"输入的时间戳需要在2030年({date_to_time_stamp(year=2030)})之前"
+        return f"输入的时间戳需要在2100年({date_to_time_stamp(year=2100)})之前"
