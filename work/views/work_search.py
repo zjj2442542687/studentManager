28292d2fab3360869af8d269e3b99770dbfdc017
zjj2442542687 <2442542687@qq.com>
2021-01-03 20:36:05 +0800

    20210103_zjj_上传作业bug修改

--- work/views/work_search.py
+++ work/views/work_search.py
@@ -6,9 +6,9 @@ from rest_framework.viewsets import GenericViewSet
 
 from classs.models import Class
 from teacher.models import Teacher
-from utils.my_info_judge import pd_adm_token, STATUS_TOKEN_NO_AUTHORITY
+from utils.my_info_judge import pd_adm_token, STATUS_TOKEN_NO_AUTHORITY, pd_token
 from utils.my_limit_offset_pagination import MyLimitOffsetPagination
-from utils.my_response import response_error_400
+from utils.my_response import response_error_400, response_success_200
 from utils.my_utils import get_class_all_id
 from work.models import Work
 from work.views.work_serializers import WorkSerializersSearch
@@ -73,12 +73,18 @@ class WorkPaginationSelectView(mixins.ListModelMixin,
         operation_summary="老师查询自己发布作业信息",
         pagination_class=None,
         manual_parameters=[
-            openapi.Parameter('teacher', openapi.IN_QUERY, type=openapi.TYPE_INTEGER, description='老师ID'),
+            openapi.Parameter('TOKEN', openapi.IN_HEADER, type=openapi.TYPE_STRING, description='老师TOKEN'),
         ]
     )
     def search_teacher(self, request, *args, **kwargs):
+        check_token = pd_token(request)
+        if check_token:
+            return check_token
+
+        if request.auth not in [0, 3]:
+            return response_success_200(code=STATUS_TOKEN_NO_AUTHORITY, message="权限不够，该token不是学生")
         # 老师
-        teacher = request.GET.get("teacher")
+        teacher = Teacher.objects.get(user=request.user).id
         work = Work.objects.filter(teacher=teacher)
 
         print(work)
@@ -91,19 +97,26 @@ class WorkPaginationSelectView(mixins.ListModelMixin,
         operation_summary="辅导员查询自己班级作业信息",
         pagination_class=None,
         manual_parameters=[
-            openapi.Parameter('clazz', openapi.IN_QUERY, type=openapi.TYPE_INTEGER,
-                              description='班级id', enum=get_class_all_id()),
             openapi.Parameter('TOKEN', openapi.IN_HEADER, type=openapi.TYPE_STRING, description='辅导员TOKEN'),
         ]
     )
     def search_clazz(self, request, *args, **kwargs):
-        teacher_pk = Class.objects.get(pk=request.GET.get('clazz')).headmaster.pk
-        if request.user is not teacher_pk:
-            print(request.user)
-            print(teacher_pk)
-            return response_error_400(status=STATUS_TOKEN_NO_AUTHORITY, message="这不是您所带班级")
-        # 班级
-        clazz = request.GET.get("clazz")
+        # teacher_pk = Class.objects.get(pk=request.GET.get('clazz')).headmaster.pk
+        # if request.user is not teacher_pk:
+        #     print(request.user)
+        #     print(teacher_pk)
+        #     return response_error_400(status=STATUS_TOKEN_NO_AUTHORITY, message="这不是您所带班级")
+        # # 班级
+        # clazz = request.GET.get("clazz")
+        # work = Work.objects.filter(clazz=clazz)
+        check_token = pd_token(request)
+        if check_token:
+            return check_token
+
+        if request.auth not in [3]:
+            return response_success_200(code=STATUS_TOKEN_NO_AUTHORITY, message="权限不够，该token不是辅导员")
+        teacher_id = Teacher.objects.get(user=request.user).id
+        clazz = Class.objects.get(headmaster=teacher_id)
         work = Work.objects.filter(clazz=clazz)
 
         print(work)
