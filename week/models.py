7e0ff109e1b847d9b3a5e2eb478eaec11963aac7
zjj2442542687 <2442542687@qq.com>
2021-01-05 20:32:31 +0800

    20210105_zjj_bug修复

--- week/models.py
+++ week/models.py
@@ -5,7 +5,7 @@ class Week(models.Model):
     index = models.SmallIntegerField('位置',
                                      choices=((1, '星期一'), (2, '星期二'), (3, '星期三'), (4, '星期四'), (5, '星期五'), (6, '星期六'),
                                               (7, '星期天')),
-                                     default=1, unique=True)
+                                     default=1, unique=False)
     title = models.CharField("标题", max_length=255, null=True)
 
     class Meta:
